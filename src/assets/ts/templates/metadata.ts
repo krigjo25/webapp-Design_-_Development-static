296202f4472cd295259074175540f2720e802eb1
Krigjo25 <krigjo25@gmail.com>
2026-05-30 12:48:30 +0200

    refactor: remove references to nonexistent socialweblogo.css and pageelement.css.

--- src/assets/ts/templates/metadata.ts
+++ src/assets/ts/templates/metadata.ts
@@ -8,7 +8,5 @@ export function getMetadata(title: string, prefix: string = ''): string {
 <meta name="keywords" content="Design, Development, media">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="http://projectmedia.net23.net/pm/portfolio/favicon/pm.png" type="favicon/ico">
-<link href="${prefix}assets/css/socialweblogo.css" rel="stylesheet" type="text/css" media="screen">
-<link href="${prefix}assets/css/pageelement.css" rel="stylesheet" type="text/css" media="screen">`;
+<link rel="shortcut icon" href="http://projectmedia.net23.net/pm/portfolio/favicon/pm.png" type="favicon/ico">`;
 }
