915408364e964359efb14573b9c0f884630f6623
Krigjo25 <krigjo25@gmail.com>
2026-05-30 16:47:28 +0200

    style: update layout tags to semantic HTML elements across subpages.

--- src/assets/ts/templates/footer.ts
+++ src/assets/ts/templates/footer.ts
@@ -1,7 +1,5 @@
 export function getFooter(prefix: string = ''): string {
   return `<footer class="site-footer">
     <p>&copy; 2026 K – Design. All rights reserved. | <a href="${prefix}ninjajs/ninja-script.html">Quiz Ninja</a></p>
-    <div class="footer-images">
-    </div>
   </footer>`;
 }
