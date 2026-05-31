29d2d1bf18417e5ea33a7799601d0d877fb3f87b
Krigjo25 <krigjo25@gmail.com>
2026-05-31 09:34:10 +0200

    feat: add template metadata, header, and footer includes to all HTML pages.

--- src/assets/ts/templates/metadata.ts
+++ src/assets/ts/templates/metadata.ts
@@ -2,11 +2,14 @@ export function getMetadata(title: string, prefix: string = ''): string {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
+<meta charset="utf-8">
 <title>${title}</title>
 <meta name="description" content="Kriss Design / Utvikling">
 <meta name="author" content="Kriss Design / Utvikling">
 <meta name="keywords" content="Design, Development, media">
-<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="http://projectmedia.net23.net/pm/portfolio/favicon/pm.png" type="favicon/ico">`;
+<link rel="shortcut icon" href="/portfolio/favicon/pm.png" type="favicon/ico">
+<link href="assets/sass/index.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+`;
 }
