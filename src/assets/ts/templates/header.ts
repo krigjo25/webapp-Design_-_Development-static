12da5cc852aaec8fae441f542a82d265f46b14e8
Krigjo25 <krigjo25@gmail.com>
2026-05-31 09:56:02 +0200

    refactor: streamline all HTML pages to leverage updated header and footer layout templates.

--- src/assets/ts/templates/header.ts
+++ src/assets/ts/templates/header.ts
@@ -1,33 +1,37 @@
+import { getMetadata } from './metadata';
+
+
 export function getHeader(prefix: string = ''): string {
-  return `<header class="header">
-<div class="weblogo">
-<a href="${prefix}index.html">
-<img src="${prefix}assets/media/images/logo/weblogo/k-mobile.png" alt="k-design/development">
-</a>
-</div>
-</a>
-</div>
-</header>
-<nav>
-  <ul>
-  <a class="link" href="${prefix}index.html">
-    <li>
-      <img src="${prefix}assets/media/images/nav/news.svg" class="news" alt="News">
-    <h4>News</h4>
-    </li>
-    </a>
-    <a class="link" href="${prefix}about.html">
-    <li>
-      <img src="${prefix}assets/media/images/nav/aboutus.svg" class="about" alt="About us">
-    <h4>About us</h4>
-    </li>
-    </a>
-    <a class="link" href="${prefix}portfolio.html">
-    <li>
-      <img src="${prefix}assets/media/images/nav/portfolio.svg" class="port" alt="Portfolio">
-    <h4>Portfolio</h4>
-    </li>
-    </a>
-  </ul>
-</nav>`;
+  return `
+${getMetadata('Kriss Design / Utvikling', prefix)}
+<body>
+  <header class="header">
+    <div class="weblogo">
+      <a href="${prefix}index.html">
+        <img src="${prefix}assets/media/images/logo/weblogo/k-mobile.png" alt="k-design/development">
+      </a>
+    </div>
+  <nav>
+    <ul>
+    <a class="link" href="${prefix}index.html">
+      <li>
+        <img src="${prefix}assets/media/images/nav/news.svg" class="news" alt="News">
+      <h4>News</h4>
+      </li>
+      </a>
+      <a class="link" href="${prefix}about.html">
+      <li>
+        <img src="${prefix}assets/media/images/nav/aboutus.svg" class="about" alt="About us">
+      <h4>About us</h4>
+      </li>
+      </a>
+      <a class="link" href="${prefix}portfolio.html">
+      <li>
+        <img src="${prefix}assets/media/images/nav/portfolio.svg" class="port" alt="Portfolio">
+      <h4>Portfolio</h4>
+      </li>
+      </a>
+    </ul>
+  </nav>
+</header>`;
 }
--- src/assets/ts/templates/metadata.ts
+++ src/assets/ts/templates/metadata.ts
@@ -3,7 +3,7 @@ export function getMetadata(title: string, prefix: string = ''): string {
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>${title}</title>
+<title>${title} | ${prefix}</title>
 <meta name="description" content="Kriss Design / Utvikling">
 <meta name="author" content="Kriss Design / Utvikling">
 <meta name="keywords" content="Design, Development, media">
