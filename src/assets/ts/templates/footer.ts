03de41f1773ffaa3deb9c9c690009eb7566f9120
Krigjo25 <krigjo25@gmail.com>
2026-05-31 09:34:50 +0200

    chore: update footer template whitespace formatting.

--- src/assets/ts/templates/footer.ts
+++ src/assets/ts/templates/footer.ts
@@ -1,7 +1,7 @@
 export function getFooter(prefix: string = ''): string {
   return `<footer class="site-footer">
     <p>&copy; 2014 K – Design. All rights reserved. | <a href="${prefix}ninjajs/ninja-script.html">Quiz Ninja</a></p>
-  </footer>
+    </footer>
   </body>
-</html>`;
+  </html>`;
 }
